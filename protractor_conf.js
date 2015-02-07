44ea0c2227ed41d05ea7b6cfba51c121460809cf
Doug Steinberg <dstein-phins@hotmail.com>
2015-02-07 17:15:45 -0500

    unit and e2e tests

--- protractor_conf.js
+++ protractor_conf.js
@@ -1,15 +1,16 @@
 // An example configuration file.
 exports.config = {
-  directConnect: true,
+  // chromeOnly: true,
+  // chromeDriver: './node_modules/protractor/selenium/chromedriver',
 
-  // Capabilities to be passed to the webdriver instance.
+  seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
+  baseUrl: 'http://0.0.0.0:9000',
+  
   capabilities: {
-    'browserName': 'firefox'
+    'browserName': 'chrome'
   },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['example_spec.js'],
+  specs: ['test/e2e/**/*_spec.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
