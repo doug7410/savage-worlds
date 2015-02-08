4da1cf9f8fe3591ee3c3f862cfa3ded990bdfe99
Doug Steinberg <dstein-phins@hotmail.com>
2015-02-08 16:36:56 -0500

    set up app to consume rails api

--- test/spec/controllers/edges.js
+++ test/spec/controllers/edges.js
@@ -2,11 +2,14 @@
 
 describe('EdgesController', function () {
 
-  var location, scope;
+  var location, scope, response, http;
 
   beforeEach(module('swFrontApp'));
 
-  beforeEach(inject(function ($controller, $rootScope, $location) {
+  beforeEach(inject(function ($controller, $rootScope, $location, $httpBackend) {
+    http = $httpBackend;
+    response = [{key: 'hello'}];
+    http.whenGET('/api/edges').respond(response);
     location = $location;
     scope = $rootScope.$new();
     $controller('EdgesController', {
@@ -14,21 +17,36 @@ describe('EdgesController', function () {
     });
   }));
 
+  afterEach(function(){
+    http.verifyNoOutstandingExpectation();
+    http.verifyNoOutstandingRequest();
+  });
+
+  it('makes request to api to fetch edges', function(){
+    http.expectGET('/api/edges');
+    http.flush();
+  });
+
+  it('it assigns response data to edges', function(){
+    http.flush();
+    expect(scope.edges[0].key).toEqual('hello');
+  });
+
   describe('[displayRequirements]', function(){
     it('[concatenates name and value of the requirement]', function(){
-      
+      http.flush();
       var reqs = [{ name: 'Agility', value: 'd6'}];
       expect(scope.displayRequirements(reqs)).toEqual('Agility d6');
     });
 
     it('[ignores name if it is null]', function(){
-      
+      http.flush();
       var reqs = [{ name: null, value: 'Novice'}];
       expect(scope.displayRequirements(reqs)).toEqual('Novice');
     });
 
     it('[requirements are delimited by a comma]', function(){
-      
+      http.flush();
       var reqs = [{ name: null, value: 'Novice'}, { name: 'Agility', value: 'd6'}];
       expect(scope.displayRequirements(reqs)).toEqual('Novice, Agility d6');
     });
